8c4875f278f1f488aee6025cc7f66b45831fe20e
Felipe Hernandez <pipeingenieria@gmail.com>
2018-09-14 14:34:29 -0500

    Creacion Condicionales y Ciclos

--- Proyecto2/index.php
+++ Proyecto2/index.php
@@ -10,8 +10,44 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h1>
+            <?php
+            // Comentarios del codigo
+            $PrimeraVariable = 2;
+            $SegundaVariable = 5;
+            $lenguaje="PHP";
+            echo $PrimeraVariable." ".$PrimeraVariable."<br>".$SegundaVariable;
+            ?>
+        </h1>
+        
+        <p>
+            <h2>
+                <?php
+                    // ----- Estructuras condicionales ----
+                   if($PrimeraVariable<1)
+                    {
+                       echo 'Soy Menor';
+                    }
+                    else
+                    {
+                        echo 'Soy Mayor';
+                    }
+                    
+                    switch ($lenguaje)
+                    {
+                        case "PHP":
+                            echo 'PHP';
+                            break;
+                        case "JavaScript":
+                            echo 'JavaScript';
+                            break;
+                        case "HTML5":
+                            echo 'HTML5';
+                            break;
+                    }
+                    //----- Arreglos ------
+                ?>
+            </h2>
+        </p>
     </body>
 </html>
