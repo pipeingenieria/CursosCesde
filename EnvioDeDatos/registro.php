83186407ec49735c933133c13a48d478afbca011
Felipe <pipeingenieria@gmail.com>
2018-09-21 13:19:12 -0500

    Creado de nuevo proyecto -- Agregado de archivos BOOTSTRAP

--- EnvioDeDatos/registro.php
+++ EnvioDeDatos/registro.php
@@ -2,8 +2,10 @@
 
     //Obtener datos get
     $usuario = $_GET;
+    $usuario1 = $_GET['nombre'];
     //var_dump($usuario);
     echo $usuario['nombre'];
+    echo $usuario1;
 /*  
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
