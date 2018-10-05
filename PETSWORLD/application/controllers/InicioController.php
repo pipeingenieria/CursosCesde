0beae9c24fe9dd7ddef68a820a53d9889d97ffd3
Felipe <pipeingenieria@gmail.com>
2018-10-05 16:45:12 -0500

    AVANCES IMPORTANTES EN PETSWORLD

--- PETSWORLD/application/controllers/InicioController.php
+++ PETSWORLD/application/controllers/InicioController.php
@@ -1,8 +1,44 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class InicioController extends CI_Controller {
 
+	
+	
+        public function inicio()
+	{
+                //Se carga el Header
+                $this->load->view('Layout/HeaderView');
+                //Se carga el Contenido de el Body
+		$this->load->view('InicioView');
+                //Se carga el Footer
+                $this->load->view('Layout/FooterView');
+	}
+        public function contacto()
+	{
+                //Se carga el Header
+                $this->load->view('Layout/HeaderView');
+                //Se carga el Contenido de el Body
+		$this->load->view('ContactoView');
+                //Se carga el Footer
+                $this->load->view('Layout/FooterView');
+	}
+        public function registro()
+	{
+                //Se carga el Header
+                $this->load->view('Layout/HeaderView');
+                //Se carga el Contenido de el Body
+		$this->load->view('RegistroView');
+                //Se carga el Footer
+                $this->load->view('Layout/FooterView');
+	}
+        public function inicioSesion()
+	{
+                //Se carga el Header
+                $this->load->view('Layout/HeaderView');
+                //Se carga el Contenido de el Body
+		$this->load->view('InicioSesionView');
+                //Se carga el Footer
+                $this->load->view('Layout/FooterView');
+	}
+}
